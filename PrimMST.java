00295e110ba8b62d67a0af498971c9c64821eb72
vmoeykens <vincent@beta.team>
2021-03-24 21:46:56 -0400

    Basic logic and display seems to work... no tree support yet

--- PrimMST.java
+++ PrimMST.java
@@ -1,11 +1,74 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 class PrimMST {
 
-    public PrimMST() {
+    public static void runPrimMST(Graph<Character> g, Vertex<Character> startVertex) {
+        ArrayList<Character> S = new ArrayList<Character>();
+        ArrayList<Edge<Character>> T = new ArrayList<Edge<Character>>();
+        g.changeKey(startVertex, 0);
+        PriorityQueue<Vertex<Character>> q = BuildPQ(g);
+        displayState(S, T, q);
+        
+        while (q.peek() != null) {
+            Vertex<Character> v = q.poll();
+            S.add(v.getId());
+            if (v.getId() != startVertex.getId()) {
+                // add the edge (pred(v), v) to T where pred is predecessor
+            }
+            LinkedList<Edge<Character>> edges = v.getEdges();
+            for (int i = 0; i < edges.size(); i++) {
+                Edge<Character> edge = edges.get(i);
+                if (!S.contains(edge.getTo())) {
+                    if (edge.weight < g.getVertices().get(g.getVertexById(edge.getTo())).getKeyVal()) {
+                        g.changeKey(g.getVertices().get(g.getVertexById(edge.getTo())), edge.weight);
+                        g.changePred(g.getVertices().get(g.getVertexById(edge.getTo())), v);
+                    }
+                }
+            }
+            displayState(S, T, q);
+        }
     }
 
     public static void main(String[] args) {
+        Graph<Character> g = makeGraph();
+        //System.out.println("Graph represented by adjacency list:\n" + g.toString());
+        runPrimMST(g, new Vertex<Character>('a'));
+
+    }
+
+    public static void displayState(ArrayList<Character> S, ArrayList<Edge<Character>> T, PriorityQueue<Vertex<Character>> q) {
+        PriorityQueue<Vertex<Character>> copyQ = new PriorityQueue<Vertex<Character>>(q); 
+        System.out.print("S = {");
+        for (int i = 0; i < S.size(); i++) {
+            System.out.print(S.get(i) + ", ");
+        }
+        System.out.print("}\n");
+
+        System.out.print("T = {");
+        for (int i = 0; i < T.size(); i++) {
+            System.out.print(T.get(i));
+        }
+        System.out.print("}\n");
+        
+        System.out.print("Q = {");
+        while (copyQ.peek() != null) {
+            System.out.print(copyQ.poll().printShort());
+        }
+        System.out.print("}\n\n");
+    }
+
+    public static PriorityQueue<Vertex<Character>> BuildPQ(Graph<Character> g) {
+        PriorityQueue<Vertex<Character>> q = new PriorityQueue<Vertex<Character>>();
+        LinkedList<Vertex<Character>> vertices = g.getVertices();
+        for (int i = 0; i < vertices.size(); i++) {
+            q.add(vertices.get(i));
+        }
+        return q;
+    }
+
+    public static Graph<Character> makeGraph() {
         Graph<Character> g = new Graph<Character>();
 
         g.addEdge('a', 'b', 9);
@@ -24,7 +87,7 @@ class PrimMST {
         g.addEdge('f', 'g', 30);
         g.addEdge('g', 'h', 44);
 
-        System.out.println("Graph represented by adjacency list:\n" + g.toString());
+        return g;
     }
 
 }
@@ -35,6 +98,22 @@ class Graph<T> {
 
     public Graph() {
         this.vertices = new LinkedList<Vertex<T>>();
+    }
+
+    public void changeKey(Vertex<T> vertex, int newKey) {
+        for (int i = 0; i < this.vertices.size(); i++) {
+            if (this.vertices.get(i).getId() == vertex.getId()){
+                this.vertices.get(i).changeKey(newKey);;
+            }
+        }
+    }
+
+    public void changePred(Vertex<T> vertex, Vertex<T> newPred) {
+        for (int i = 0; i < this.vertices.size(); i++) {
+            if (this.vertices.get(i).getId() == vertex.getId()){
+                this.vertices.get(i).setPredecessor(newPred);;
+            }
+        }
     }
 
     public void addEdge(T from, T to, int weight) {
@@ -66,6 +145,10 @@ class Graph<T> {
         return false;
     }
 
+    public LinkedList<Vertex<T>> getVertices() {
+        return this.vertices;
+    }
+
     public int getVertexById(T id) {
         for (int i = 0; i < this.vertices.size(); i++) {
             if (this.vertices.get(i).getId() == id) {
@@ -86,14 +169,17 @@ class Graph<T> {
 
 }
 
-class Vertex<T> {
+class Vertex<T> implements Comparable<Vertex<T>>{
     T id;
+    int keyVal;
     LinkedList<Edge<T>> edges;
+    Vertex<T> pred;
 
 
     public Vertex(T id) {
         this.id = id;
         this.edges = new LinkedList<Edge<T>>();
+        this.keyVal = Integer.MAX_VALUE;
     }
 
     public boolean addEdge(Edge<T> edge) {
@@ -105,8 +191,24 @@ class Vertex<T> {
         }
     }
 
+    public void setPredecessor(Vertex<T> pred) {
+        this.pred = pred;
+    }
+
     public T getId() {
         return this.id;
+    }
+
+    public int getKeyVal() {
+        return this.keyVal;
+    }
+
+    public LinkedList<Edge<T>> getEdges() { 
+        return this.edges;
+    }
+
+    public void changeKey(int newKey) {
+        this.keyVal = newKey;
     }
 
     @Override
@@ -116,6 +218,15 @@ class Vertex<T> {
             outputString += this.edges.get(i).toString() + ", "; 
         }
         return outputString;
+    }
+
+    public String printShort() {
+        return "(" + this.getId() + ", " + this.getKeyVal() + "), ";
+    }
+
+    @Override
+    public int compareTo(Vertex<T> otherVertex) {
+        return this.getKeyVal() - otherVertex.getKeyVal();
     }
 
 }
